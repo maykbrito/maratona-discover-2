6ce7a2b270cb8e45e00d79280ad8ddb12ea9e618
Mayk Brito <maykbrito@gmail.com>
2021-03-20 16:41:54 -0300

    03
    - nodemon
    - enviar index.html
    - diretório absoluto, raiz
    - __dirname

--- src/server.js
+++ src/server.js
@@ -10,4 +10,14 @@ const server = express()
 // Response: resposta do servidor
 // 2. server.get('/', (request, response) => response.send('mensagem de retorno'))
 
+
+// Enviar .html
+server.get('/', (request, response) => {
+
+  // caminho precisa ser absoluto até o diretório raiz
+  // o que é caminho absoluto?
+  // o que é diretório raiz?
+  response.sendFile(__dirname + '/index.html')
+})
+
 server.listen(3000, () => console.log('server on'))
