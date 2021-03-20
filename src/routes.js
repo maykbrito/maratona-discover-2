54368cd780034d69cc078550fde9046542ebefa4
Mayk Brito <maykbrito@gmail.com>
2021-03-20 18:06:59 -0300

    07
    - programar html
    - variaveis ejs
    - .toFixed
    - .replace
    - objeto user profile
    - enviar objeto ao html/ejs

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,21 @@ const routes = express.Router()
 
 const views = __dirname + '/views'
 
+const profile = {
+  name: "Jakeliny",
+  avatar: "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
+  "montly-amount": 3000,
+  "hours-per-day": 4,
+  "days-per-week": 5,
+  "vacation-per-year": 4,
+  "value-hour": 75
+}
+
 routes
   .get('/', (req, res) => res.render(views + '/index'))
-  .get('/profile', (req, res) => res.render(views + '/profile'))
+  .get('/profile', (req, res) => 
+    res.render(views + '/profile', { profile })
+  )
   .get('/job', (req, res) => res.render(views + '/job'))
   .get('/job/edit', (req, res) => res.render(views + '/job-edit'))
 
