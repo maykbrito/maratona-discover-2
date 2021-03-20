ad5fd4e9b131acf03dc252e8cca5facc9df8ce24
Mayk Brito <maykbrito@gmail.com>
2021-03-20 16:30:07 -0300

    02
    - primeira rota
    - metodos http (GET)
    - request e response

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const server = express()
 
+// Métodos HTTP => Mostrar como acessar ao curso 
+// GET: Pegar um recurso
+// 1. server.get('/', () => console.log('cheguei na rota'))
+
+// Request e Response
+// Request: pedido ao servidor
+// Response: resposta do servidor
+// 2. server.get('/', (request, response) => response.send('mensagem de retorno'))
+
 server.listen(3000, () => console.log('server on'))
