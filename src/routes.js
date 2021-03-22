b7e07f5f14c64ffe93f57f8917561447a3e3ac38
Mayk Brito <maykbrito@gmail.com>
2021-03-22 08:40:00 -0300

    03
    - [ ]  Configurar jobs na criação de um novo Job
        - [ ] job id: pegar o id do ultimo item do array
        - [ ]  created_at: Date.now()
            - [ ]  para o cálculo de dias restantes
        - [ ]  total-hours
        - [ ]  daily-hours
        - [ ]  name
    - [ ]  atualizar os jobs no index, antes de apresentar
        - [ ]  remaining calculation
            - [ ]  Devemos calcular sempre que apresentar o projeto, pois poderemos mudar os dados do projeto a qualquer momento
            - [ ]  remainingDays = total hours do job / daily hours do job
            - [ ]  adicionar os dias à data de criação, para criar uma data futura
            - [ ]  subtrair da data futura, o número de dias restantes baseado na data de hoje
            - [ ]  pegar a diferença de milissegundos para dias
        - [ ]  update status (done | progress)
        - [ ]  budget: profile value hour * total job hours
            - [ ]  deverá ser atualizado sempre que apresentar o projeto, pois poderá variar dependendo dos dados do projeto ou dos dados do perfil
    - [ ]  atualizar o index.ejs
        - [ ]  Prazo encerrado ao invés de 0 dias para a entrega
    - [ ]  adicionar uma entrada no jobs[]  para o prazo encerrado

--- src/routes.js
+++ src/routes.js
@@ -3,44 +3,127 @@ const routes = express.Router()
 
 const views = __dirname + '/views'
 
-const profile = {
-  name: "Jakeliny",
-  avatar: "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
-  "monthly-budget": 3000,
-  "hours-per-day": 4,
-  "days-per-week": 5,
-  "vacation-per-year": 4,
-  "value-hour": 75
+// Organizar os dados e funções, em um Object Literal, para a entidade Profile
+const Profile = {
+  data: {
+    name: "Jakeliny",
+    avatar: "https://avatars.githubusercontent.com/u/17316392?s=460&u=6912a91a70bc89745a2079fdcdad3bc3ce370f13&v=4",
+    "monthly-budget": 3000,
+    "hours-per-day": 4,
+    "days-per-week": 5,
+    "vacation-per-year": 4,
+    "value-hour": 75
+  },
+  controller: {
+    index(req, res) {
+      res.render(views + '/profile', { profile: Profile.data })
+    },
+    update(req, res) {
+      const data = req.body
+
+      const weeksPerYear = 52
+      const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+      const weekTotalHours =  data["hours-per-day"] * data["days-per-week"] 
+      const monthlyTotalHours = weekTotalHours * weeksPerMonth
+      data["value-hour"] = data["montly-amount"] / monthlyTotalHours
+
+      Profile.data = data
+
+      res.redirect("/profile")
+    },
+  }
 }
 
-const jobs = [
-  {
-    id: 1,
-    name: "Pizzaria Guloso",
-    status: "progress",
-    total: 4500,
-    remaining: 3
+// Organizar os dados e funções, em um Object Literal, para a entidade Job
+const Job = {
+  data: [
+    {
+      id: 1,
+      name: "Pizzaria Guloso",
+      "total-hours": 60,
+      "daily-hours": 2,
+      created_at: Date.now()
+    },
+    {
+      id: 2,
+      name: "OneTwo Project",
+      "total-hours": 13,
+      "daily-hours": 1,
+      created_at: Date.now()
+    },
+    { // prazo encerrado
+      id: 3,
+      name: "File Project",
+      "total-hours": 0,
+      "daily-hours": 1,
+      created_at: Date.now()
+    },
+  ], 
+  controller: {
+    index(req, res){
+      let updateJobs = Job.data.map(job => {
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+  
+        const date = new Date(job.created_at)
+        const dueDay = date.getDate() + Number(remainingDays)
+        const dueDate = date.setDate(dueDay)
+  
+        const timeDiffInMs = dueDate - Date.now()
+        const dayInMs = 1000 * 60 * 60 * 24
+        const dayDiff = (timeDiffInMs / dayInMs).toFixed()
+  
+        const status = dayDiff <= 0 ? "done" : "progress"
+  
+        const budget = Job.service.calculateBudget(job)
+  
+        return {
+          ...job, // espalhar os items do job dentro desse novo objeto
+          budget,
+          status,
+          remaining: dayDiff,
+        }
+      })
+  
+      res.render(views + '/index', { jobs: updateJobs })
+    },
+    create(req, res){
+      const lastId = Job.data[Job.data.length - 1].id
+
+      Job.data.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "total-hours": req.body["total-hours"],
+        "daily-hours": req.body["daily-hours"],
+        created_at: Date.now()
+      }) // colocar os dados em uma variável
+  
+      res.redirect('/') // redireicionar a página para o home
+    },
+    show(req, res) {
+      const jobId = req.params.id
+
+      const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+      if(!job) {
+        return res.send("Job not found!")
+      }
+
+      job.budget = Job.service.calculateBudget(job).toFixed(2).replace(".", ",")
+
+      res.render(views + '/job-edit', { job })
+    }
   },
-  {
-    id: 2,
-    name: "OneTwo Project",
-    status: "done",
-    total: 4500,
-    remaining: 3
-  },
-]
+  service: {
+    calculateBudget: (job) => Profile.data["value-hour"] * job["total-hours"]
+  }
+}
 
 routes
-  .get('/', (req, res) => res.render(views + '/index', { jobs }))
-  .get('/profile', (req, res) => 
-    res.render(views + '/profile', { profile })
-  )
+  .get('/', Job.controller.index)
+  .get('/profile', Profile.controller.index)
+  .post('/profile', Profile.controller.update)
   .get('/job', (req, res) => res.render(views + '/job'))
-  .post('/job', (req, res) => {
-    jobs.push(req.body) // colocar os dados em uma variável
-
-    res.redirect('/') // redireicionar a página para o home
-  })
-  .get('/job/edit', (req, res) => res.render(views + '/job-edit'))
+  .post('/job', Job.controller.create)
+  .get('/job/:id', Job.controller.show)
 
 module.exports = routes
