cc718aea22f3120ebc2b0eeecf1d1e4594d8f425
Mayk Brito <maykbrito@gmail.com>
2021-03-23 06:34:47 -0300

    fix monthly profile and url /job/delete

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,6 @@ for (let card of cards) {
   const deleteButton = card.querySelector('button.delete')
   deleteButton.onclick = () => {
     modal.open()
-    deleteForm.setAttribute('action', '/delete/' + cardId)
+    deleteForm.setAttribute('action', '/job/delete/' + cardId)
   }
 }
